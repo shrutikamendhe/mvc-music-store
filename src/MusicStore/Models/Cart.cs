a56f8db49eef92dc5393837fca4a44a84d113193
Shrutika Mendhe <shrutika.mendhe@click2cloud.net>
2016-05-17 12:54:08 +0530

    added code

---
+++ src/MusicStore/Models/Cart.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MusicStore.Models
+{
+    public class Cart
+    {
+        public int RecordId { get; set; }
+
+        [Required]
+        public string CartId { get; set; }
+        public int AlbumId { get; set; }
+        public int Count { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime DateCreated { get; set; }
+
+        public virtual Album Album { get; set; }
+    }
+}
